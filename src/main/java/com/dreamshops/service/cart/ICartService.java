60cb4b67ff3deab3880f51da162fbc3aee588489
TeNiReY <tenirey11@gmail.com>
2025-05-04 18:21:38 +0300

    -added and implemented CartService.java

---
+++ src/main/java/com/dreamshops/service/cart/ICartService.java
@@ -0,0 +1,11 @@
+package com.dreamshops.service.cart;
+
+import com.dreamshops.model.Cart;
+
+import java.math.BigDecimal;
+
+public interface ICartService {
+    Cart getCart(Long id);
+    void clearCart(Long id);
+    BigDecimal getTotalPrice(Long id);
+}
