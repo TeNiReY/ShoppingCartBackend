61fe87cb2248b6d4ae495e832d0e665d46e0094f
TeNiReY <tenirey11@gmail.com>
2025-05-08 17:34:29 +0300

    -added and implemented OrderService

---
+++ src/main/java/com/dreamshops/service/order/IOrderService.java
@@ -0,0 +1,12 @@
+package com.dreamshops.service.order;
+
+import com.dreamshops.model.Order;
+
+import java.util.List;
+
+public interface IOrderService {
+    Order placeOrder(Long userId);
+    Order getOrder(Long orderId);
+
+    List<Order> getUserOrders(Long userId);
+}
---
+++ src/main/java/com/dreamshops/service/order/OrderService.java
@@ -0,0 +1,81 @@
+package com.dreamshops.service.order;
+
+import com.dreamshops.enums.OrderStatus;
+import com.dreamshops.exceptions.ResourceNotFoundException;
+import com.dreamshops.model.Cart;
+import com.dreamshops.model.Order;
+import com.dreamshops.model.OrderItem;
+import com.dreamshops.model.Product;
+import com.dreamshops.repository.OrderRepository;
+import com.dreamshops.repository.ProductRepository;
+import com.dreamshops.service.cart.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService implements IOrderService {
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final CartService cartService;
+
+    @Override
+    public Order placeOrder(Long userId) {
+        Cart cart = cartService.getCartByUserId(userId);
+
+        Order order = createOrder(cart);
+        List<OrderItem> orderItemList = createOrderItems(order, cart);
+        order.setOrderItems(new HashSet<>(orderItemList));
+        order.setOrderTotalAmount(calculateTotalAmount(orderItemList));
+        Order savedOrder = orderRepository.save(order);
+        cartService.clearCart(cart.getId());
+
+        return savedOrder;
+    }
+
+    private Order createOrder(Cart cart) {
+        Order order = new Order();
+        order.setUser(cart.getUser());
+        order.setOrderStatus(OrderStatus.PENDING);
+        order.setOrderDate(LocalDate.now());
+        return order;
+    }
+
+    private List<OrderItem> createOrderItems(Order order, Cart cart) {
+        return cart.getItems().stream().map(cartItem -> {
+            Product product = cartItem.getProduct();
+            product.setInventory(product.getInventory() - cartItem.getQuantity());
+            productRepository.save(product);
+            return new OrderItem(
+                    order,
+                    product,
+                    cartItem.getQuantity(),
+                    cartItem.getUnitPrice());
+        }).toList();
+    }
+
+    private BigDecimal calculateTotalAmount(List<OrderItem> orderItemList) {
+        return orderItemList
+                .stream()
+                .map(item -> item.getPrice()
+                        .multiply(new BigDecimal(item.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+    }
+
+    @Override
+    public Order getOrder(Long orderId) {
+        return orderRepository.findById(orderId)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found"));
+    }
+
+    @Override
+    public List<Order> getUserOrders(Long userId) {
+        return orderRepository.findByUserId(userId);
+    }
+}
